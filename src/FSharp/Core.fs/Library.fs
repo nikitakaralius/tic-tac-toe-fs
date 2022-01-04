c217c1b23e3577754ac1b2ae160133319a4ea136
nikitakaralyus <nikitakaralyus@gmail.com>
2022-01-04 22:43:49 +0400

    Created game initialization in game component

--- src/FSharp/Core.fs/Library.fs
+++ src/FSharp/Core.fs/Library.fs
@@ -2,7 +2,13 @@
 
 open TMPro
 open UnityEngine
-open UnityEngine.UIElements
+open UnityEngine.Events
+open UnityEngine.UI
+open TicTackToeDomain
+
+type Update =
+    | None
+    | Action of Action
 
 [<AllowNullLiteral>]
 type GameComponent() =
@@ -19,7 +25,47 @@ type GameComponent() =
     [<DefaultValue>]
     [<SerializeField>]
     val mutable private stateLabel: TextMeshProUGUI
+    
+    let startEvent = Event<_>()
+    let updateEvent = Event<Update>()
+    
+    member private this.Start() = startEvent.Trigger()
+    member private this.Update() = updateEvent.Trigger(None)
+    
+    member private this.StartAsync() = Async.AwaitEvent startEvent.Publish
+    member private this.UpdateAsync() = Async.AwaitEvent updateEvent.Publish
+    
+    member private this.Awake() =
+        let gameFlow = async {
+            do! this.StartAsync()
+            
+            this.restartButton.onClick.AddListener(UnityAction(
+                this.OnRestartButtonClicked))
+            
+            let initialGame = init()
+            
+            let rec gameLoop currentGame = async {
+                let! updateType = this.UpdateAsync()
+                
+                let game = match updateType with
+                            | None -> currentGame
+                            | Action action -> update currentGame action
+                            
+                this.UpdateView game
+                
+                return! gameLoop game
+            }
+            
+            return! gameLoop initialGame
+        }
+        gameFlow |> Async.StartImmediate |> ignore
 
+    member private this.UpdateView game =
+        ()
+    
+    member private this.OnRestartButtonClicked() =
+        updateEvent.Trigger (Action Restart)
+    
 and FieldComponent() =
     inherit MonoBehaviour()
     
