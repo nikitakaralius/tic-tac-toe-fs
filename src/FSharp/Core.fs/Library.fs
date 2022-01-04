7f19a4f3f916c6d5e580d717355c3082aa3c7e2b
nikitakaralyus <nikitakaralyus@gmail.com>
2022-01-04 21:05:42 +0400

    Created game bcl

--- src/FSharp/Core.fs/Library.fs
+++ src/FSharp/Core.fs/Library.fs
@@ -1,7 +1,53 @@
-﻿namespace FSharp
+﻿namespace TickTackTie.Core
 
+open TMPro
 open UnityEngine
+open UnityEngine.UIElements
 
-type FSharpLog() =
+[<AllowNullLiteral>]
+type Game() =
     inherit MonoBehaviour()
-    member _.Start() = Debug.Log("Hello from F# class")
+    
+    [<DefaultValue>]
+    [<SerializeField>]
+    val mutable private field: Field
+
+    [<DefaultValue>]
+    [<SerializeField>]
+    val mutable private restartButton: Button
+    
+    [<DefaultValue>]
+    [<SerializeField>]
+    val mutable private stateLabel: TextMeshProUGUI
+
+and Field() =
+    inherit MonoBehaviour()
+    
+    [<DefaultValue>]
+    [<SerializeField>]
+    val mutable private xSprite: Sprite
+    
+    [<DefaultValue>]
+    [<SerializeField>]
+    val mutable private oSprite: Sprite
+
+    [<DefaultValue>]
+    [<SerializeField>]    
+    val mutable private cells: Cell[]
+
+and [<RequireComponent(typedefof<Collider2D>)>]
+    [<RequireComponent(typedefof<SpriteRenderer>)>]
+    Cell() =
+        inherit MonoBehaviour()
+        
+        [<DefaultValue>]
+        [<SerializeField>]
+        val mutable private field: Field
+        
+        [<DefaultValue>]
+        [<SerializeField>]
+        val mutable private collider: Collider2D
+        
+        [<DefaultValue>]
+        [<SerializeField>]
+        val mutable private renderer: SpriteRenderer
