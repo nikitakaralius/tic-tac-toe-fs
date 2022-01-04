f2b6d0208731e235330cc7fce258c3c530ee0945
nikitakaralyus <nikitakaralyus@gmail.com>
2022-01-04 23:12:12 +0400

    Cretead Game Core

--- src/FSharp/Core.fs/Library.fs
+++ src/FSharp/Core.fs/Library.fs
@@ -1,5 +1,6 @@
 ﻿namespace TicTackToe.Core
 
+open System
 open TMPro
 open UnityEngine
 open UnityEngine.Events
@@ -35,6 +36,9 @@ type GameComponent() =
     member private this.StartAsync() = Async.AwaitEvent startEvent.Publish
     member private this.UpdateAsync() = Async.AwaitEvent updateEvent.Publish
     
+    member this.DoMoveAt position =
+        updateEvent.Trigger (Action (Move position))
+    
     member private this.Awake() =
         let gameFlow = async {
             do! this.StartAsync()
@@ -61,7 +65,15 @@ type GameComponent() =
         gameFlow |> Async.StartImmediate |> ignore
 
     member private this.UpdateView game =
-        ()
+        Array.iteri (fun index cell ->
+            this.field.DrawCell (indexToPosition index) cell game.State)
+            game.Board
+            
+        match game.State with
+        | Playing -> this.stateLabel.text <- $"{game.Current.ToString()}'s turn"
+        | GameOver result -> match result with
+                             | Win player -> this.stateLabel.text <- $"{player.ToString()} wins!"
+                             | Draw -> this.stateLabel.text <- "It's a draw!"
     
     member private this.OnRestartButtonClicked() =
         updateEvent.Trigger (Action Restart)
@@ -80,6 +92,23 @@ and FieldComponent() =
     [<DefaultValue>]
     [<SerializeField>]    
     val mutable private cells: CellComponent[]
+    
+    [<DefaultValue>]
+    [<SerializeField>]    
+    val mutable private game: GameComponent
+    
+    member this.DrawCell (x, y) cell gameState =
+        let cellSprite = match cell with
+                         | Empty -> null
+                         | Occupied player -> match player with
+                                              | X -> this.xSprite
+                                              | O -> this.oSprite
+        this.cells.[positionToIndex (x, y)].Draw cellSprite gameState
+        
+    member this.Draw cell =
+        let index = this.cells |> Array.findIndex ((=) cell)
+        let position = indexToPosition index
+        this.game.DoMoveAt position
 
 and [<RequireComponent(typedefof<Collider2D>)>]
     [<RequireComponent(typedefof<SpriteRenderer>)>]
@@ -97,3 +126,10 @@ and [<RequireComponent(typedefof<Collider2D>)>]
         [<DefaultValue>]
         [<SerializeField>]
         val mutable private renderer: SpriteRenderer
+        
+        member this.Draw cellSprite gameState =
+            this.renderer.sprite <- cellSprite
+            this.collider.enabled <- isNull cellSprite && gameState = Playing
+            
+        member private this.OnMouseDown() =
+            this.field.Draw this
